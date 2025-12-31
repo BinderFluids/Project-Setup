a8745dcd698d430c814bb309831fd5fe9357b932
Tanner Martin <79233634+BinderFluids@users.noreply.github.com>
2025-12-31 12:34:43 -0700

    Updates

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## [1.0.0] - 12/30/2025
 ### First Release
 - Create Project Setup Package
+
+## [1.0.1] - 12/31/2025
+### Added UniTask
+- Added UniTask git URL to ProjectSetup.cs file
--- Editor/ProjectSetup.cs
+++ Editor/ProjectSetup.cs
@@ -24,7 +24,8 @@ public static class ProjectSetup {
             "git+https://github.com/BinderFluids/Unity-Scriptable-Variables.git",
             "git+https://github.com/adammyhre/Unity-Utils.git",
             "git+https://github.com/adammyhre/Unity-Improved-Timers.git",
-            "git+https://github.com/KyleBanks/scene-ref-attribute.git"
+            "git+https://github.com/KyleBanks/scene-ref-attribute.git",
+            "git+https://github.com/Cysharp/UniTask.git"
         });
     }
 
